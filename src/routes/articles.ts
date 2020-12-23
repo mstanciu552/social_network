02b7def184d7b4ea8efa55e389a96fac49fd0dee
mstanciu552 <mstanciu552@gmail.com>
2020-12-23 23:03:35 +0200

    added articles routes

--- src/routes/articles.ts
+++ src/routes/articles.ts
@@ -3,15 +3,15 @@ import { db } from "../config.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  await db.query("select * from articles;", (err, result) => {
+router.get("/", (req, res) => {
+  db.query("select * from articles", (err, result) => {
     if (err) throw err;
     res.send(result);
   });
 });
 
-router.get("/:id", async (req, res) => {
-  await db.query(
+router.get("/:id", (req, res) => {
+  db.query(
     `select * from articles where id=${req.params.id}`,
     (err, result) => {
       if (err) throw err;
@@ -20,19 +20,55 @@ router.get("/:id", async (req, res) => {
   );
 });
 
-router.post("/", async (req, res) => {
+router.post("/", (req, res) => {
   const article = req.body;
-  await db.query(
-    `insert into articles(title, body) values (${article.title}, ${article.body})`,
+  db.query(
+    `insert into articles(title, body, author) values ('${article.title}', '${article.body}', '${article.author}')`,
     (err, result) => {
       if (err) throw err;
       console.log(result);
+      return res.send("Article added");
     }
   );
 });
 
-router.put("/:id", async (req, res) => {});
+router.put("/:id", (req, res) => {
+  const article = req.body;
+  db.query(
+    `update articles set title='${article.title}', body='${article.body}' where id=${req.params.id}`,
+    (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      return res.send("Article edited");
+    }
+  );
+});
 
-router.delete("/:id", async (req, res) => {});
+router.delete("/:id", (req, res) => {
+  db.query(`delete from articles where id=${req.params.id}`, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    return res.send("Article deleted");
+  });
+});
+
+router.get("/user/:id", (req, res) => {
+  db.query(
+    `select title, body from articles where articles.author=(select id from users where id=${req.params.id})`,
+    (err, result) => {
+      if (err) throw err;
+      return res.send(result);
+    }
+  );
+});
+
+// Dev utilities
+router.delete("/", (req, res) => {
+  db.query(`delete from articles`, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    return res.send("All articles deleted");
+  });
+});
 
 export default router;
